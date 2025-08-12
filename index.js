c2876bb73171be8ad9a01fac1c39b5df161221cb
Lucas Castro <lc95724@gmail.com>
2025-08-12 19:22:08 +0000

    Exercício 1 com parâmetros

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 alert("Digite a seguir dois números que representam os lados de um retângulo");
 
 
-base_num = parseFloat(prompt("Digite o primeiro número: "));
-altura_num = parseFloat(prompt("Digite o segundo número: "));
+const base_num = parseFloat(prompt("Digite o primeiro número: "));
+const altura_num = parseFloat(prompt("Digite o segundo número: "));
 const arearetangulo = base_num * altura_num;
 
-function areaTotal(){
+function areaTotal(b, a){
 
-    document.getElementById("area_retangulo").innerHTML = arearetangulo;
+    document.getElementById("area_retangulo").innerHTML = ("A base e altura são: " + b + " e " + a + " e sua área é: " + arearetangulo);
 
 };
