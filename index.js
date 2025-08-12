8aa8cf1c58f051d8e684903c5a743dbc658b7967
Lucas Castro <lc95724@gmail.com>
2025-08-12 17:58:32 +0000

    Primeira linha - exercício 1

---
+++ index.js
@@ -0,0 +1,11 @@
+alert("Digite a seguir dois números que representam os lados de um retângulo")
+
+base_num = parseFloat(prompt("Digite o primeiro número: "));
+altura_num = parseFloat(prompt("Digite o segundo número: "));
+area_retangulo = base_num * altura_num 
+
+function areaTotal(){
+
+    document.innerHTML(area_retangulo) = area_retangulo
+
+};
