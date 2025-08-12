ad6cbbcc0776a95dcb238f229a3bddc0d0f51a4e
Lucas Castro <lc95724@gmail.com>
2025-08-12 18:26:27 +0000

    Linha final - exercício 1

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-alert("Digite a seguir dois números que representam os lados de um retângulo")
+
+alert("Digite a seguir dois números que representam os lados de um retângulo");
+
 
 base_num = parseFloat(prompt("Digite o primeiro número: "));
 altura_num = parseFloat(prompt("Digite o segundo número: "));
-area_retangulo = base_num * altura_num 
+const arearetangulo = base_num * altura_num;
 
 function areaTotal(){
 
-    document.getElementById("area_retangulo").innerHTML = area_retangulo
+    document.getElementById("area_retangulo").innerHTML = arearetangulo;
 
 };
