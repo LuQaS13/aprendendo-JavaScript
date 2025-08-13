dbfc136b42fbd9e8084f4d8923afb2506a57c689
Lucas Castro <lc95724@gmail.com>
2025-08-13 19:52:25 +0000

    Exercício 1 com verificação de valores

--- index.js
+++ index.js
@@ -6,8 +6,13 @@ const base_num = parseFloat(prompt("Digite o primeiro número: "));
 const altura_num = parseFloat(prompt("Digite o segundo número: "));
 const arearetangulo = base_num * altura_num;
 
+if (isNaN(base_num) || isNaN(altura_num)){
+    alert("Valor incorreto inserido, favor verificar e tentar novamente!")
+} else {
+
 function areaTotal(b, a){
 
     document.getElementById("area_retangulo").innerHTML = ("A base e altura são: " + b + " e " + a + " e sua área é: " + arearetangulo);
 
 };
+}
